6bf2b75237487e52f15ecfa3923cf2c589ec1108
Vlasko <luka.j.v@gmail.com>
2020-04-07 17:41:30 +0100

    add for loop for 5 hours of data pulling

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 # Standard Library Imports
+from time import sleep
 
 # Third Party Imports
 import busio
@@ -21,16 +22,19 @@ cs = digitalio.DigitalInOut(board.D5)
 # create the mcp object
 mcp = MCP.MCP3008(spi, cs)
 
-# create an analog input channel on pin 0
-moistureSensor = AnalogIn(mcp, MCP.P0)
-# output signal 0-4.2 V
-# 0 ~300    : dry soil :      0~1.33 V
-# 300~700   : humid soil :    1.33~3.09 V
-# 700~950   : in water :      3.09~4.20 V
-write_data('moisture', moistureSensor.voltage, moistureSensor.value)
+def action():
+    # create an analog input channel on pin 0
+    moistureSensor = AnalogIn(mcp, MCP.P0)
+    # output signal 0-4.2 V
+    # 0 ~300    : dry soil :      0~1.33 V
+    # 300~700   : humid soil :    1.33~3.09 V
+    # 700~950   : in water :      3.09~4.20 V
+    write_data('moisture', moistureSensor.voltage, moistureSensor.value)
 
-# create an analog input channel on pin 1
-liquidlevelSensor = AnalogIn(mcp, MCP.P1)
-write_data('liquidlevel', liquidlevelSensor.voltage, liquidlevelSensor.value)
+    # create an analog input channel on pin 1
+    liquidlevelSensor = AnalogIn(mcp, MCP.P1)
+    write_data('liquidlevel', liquidlevelSensor.voltage, liquidlevelSensor.value)
 
-write_data('liquidlevel',1,2)
+for i in range(0,300):
+    action()
+    sleep(60)
