5fa8db2c35fb85369452654f3936ec91e6884fec
Vlasko <luka.j.v@gmail.com>
2020-04-29 17:41:27 +0100

    Add print statement for cron

--- watering.py
+++ watering.py
@@ -1,8 +1,10 @@
 # Standard Library Imports
-
+from datetime import datetime
 # Third Party Imports
 
 # Local Application Imports
 from functions.relay_control import switch
 
 switch(10,14)
+
+print('Plant watered at',datetime.now())
