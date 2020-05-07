ce7be34885bdfdb8429c91f59f0fa990ddd9a3ab
Vlasko <luka.j.v@gmail.com>
2020-05-07 12:47:18 +0100

    Fix bugs

--- watering.py
+++ watering.py
@@ -1,12 +1,26 @@
 # Standard Library Imports
 from datetime import datetime
 # Third Party Imports
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
 
 # Local Application Imports
 from functions.relay_control import switch
 from functions.writing_functions import read_sensor
 
-if read_sensor > 1.90:
+# Setup
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D5)
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+moistureSensor= AnalogIn(mcp, MCP.P0)
+
+if read_sensor(moistureSensor,'moisture') > 1.90:
     switch(3,14)
     print('Plant watered at',datetime.now())
 else:
